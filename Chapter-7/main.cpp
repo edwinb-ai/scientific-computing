bcdf26a1715f1c9ba07a4eea98c8f27d8af05e5c
developEdwin <developeredwin@gmail.com>
2016-01-09 17:45:27 -0600

    Modified main.cpp to include Guest class examples

--- Chapter-7/main.cpp
+++ Chapter-7/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-#include "Book.hpp"
+// #include "Ebook.h"	// Uncomment for Ebook example use
+#include "Guest.h"		// Uncomment for Guest examples use
+#include "SpecialGuest.h"
 
 int main()
 {
-    Book holiday_reading;
+	/* 
+	 *=== Ebook example ===*
+
+    Ebook holiday_reading;
     holiday_reading.title = "The skull beneath the skin";
     holiday_reading.author = "P D James";
 
@@ -18,8 +23,34 @@ int main()
     std::cout << "Year of publication is " << holiday_reading.GetYearOfPublication()
             << "\n";
 
-//    holiday_reading.hiddenUrl = "http://ebook.example.com/example-ebook";
-//    std::cout << "The URL is " << holiday_reading.hiddenUrl << "\n";
+    holiday_reading.hiddenUrl = "http://ebook.example.com/example-ebook";
+    std::cout << "The URL is " << holiday_reading.hiddenUrl << "\n";
 
     return 0;
+	*/
+
+	Guest* p_gu1 = new Guest;
+	Guest* p_gu2 = new Guest;
+	Guest* p_gu3 = new SpecialGuest;
+
+	//Set the three guests identically
+	p_gu1->numberOfNights = 3;
+	p_gu1->telephoneBill = 0.00;
+	p_gu2->numberOfNights = 3;
+	p_gu2->telephoneBill = 0.00;
+	p_gu3->numberOfNights = 3;
+	p_gu3->telephoneBill = 0.00;
+
+	std::cout << "Bill for guest 1 = "
+		<< p_gu1->CalculateBill() << std::endl;
+	std::cout << "Bill for guest 2 = "
+		<< p_gu2->CalculateBill() << std::endl;
+	std::cout << "Smaller bill for guest 3 = "
+		<< p_gu3->CalculateBill() << std::endl;
+
+	delete p_gu1;
+	delete p_gu2;
+	delete p_gu3;
+
+	return 0;
 }
